d1755cec7d8bdbd9800cd99a692d8f74a0d5beff
naichenardi <naichenardi@gmail.com>
2022-11-13 20:02:01 +0000

    Adding swagger dependencies on project and configuring some basic things to make clear the documentation.

---
+++ src/main/java/com/korber/myfilter/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.korber.myfilter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My Filter API",
+                "API documentation to use My Filter service.",
+                "0.0.1",
+                "",
+                new Contact("", "", ""),
+                "",
+                "",
+                new ArrayList<>());
+    }
+}
