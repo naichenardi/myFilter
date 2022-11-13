d2ee66916e10c9267c314166fb9c9d17af87eae7
naichenardi <naichenardi@gmail.com>
2022-11-13 21:20:07 +0000

    Change swagger to use open API 3 specification

--- src/main/java/com/korber/myfilter/MyFilterApplication.java
+++ src/main/java/com/korber/myfilter/MyFilterApplication.java
@@ -2,10 +2,8 @@ package com.korber.myfilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class MyFilterApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/korber/myfilter/config/SwaggerConfig.java
+++ src/main/java/com/korber/myfilter/config/SwaggerConfig.java
@@ -1,37 +1,18 @@
 package com.korber.myfilter.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.ArrayList;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public Docket postsApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "My Filter API",
-                "API documentation to use My Filter service.",
-                "0.0.1",
-                "",
-                new Contact("", "", ""),
-                "",
-                "",
-                new ArrayList<>());
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("My Filter API doc")
+                .description("This is a API documentation for Korber tech challenge.")
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
