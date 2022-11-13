a9479020720f51fe1f965e1950766840355d3c16
naichenardi <naichenardi@gmail.com>
2022-11-13 23:25:38 +0000

    Create models repositories. - MyFilterRepository.java - ScreenRepository.java - UserRepository.java

---
+++ src/main/java/com/korber/myfilter/db/repositories/MyFilterRepository.java
@@ -0,0 +1,11 @@
+package com.korber.myfilter.db.repositories;
+
+import com.korber.myfilter.db.model.MyFilter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MyFilterRepository extends JpaRepository<MyFilter, UUID> {
+}
