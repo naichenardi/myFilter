59d924f0f31c54869024b07ab4baffdf52a676c8
naichenardi <naichenardi@gmail.com>
2022-11-13 16:50:34 +0000

    Initial commit

---
+++ src/main/java/com/korber/myfilter/MyFilterApplication.java
@@ -0,0 +1,13 @@
+package com.korber.myfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korber/myfilter/MyFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korber.myfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
